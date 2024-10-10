7905783e422dffa7762de12842fe7a47092bce15
manibhumaraju <manishvarmab@gmail.com>
2024-10-10 20:24:39 +0530

    terraform

--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,6 @@
 module "vpc" {
     source = "../terraform-aws-vpc"
+    # source = "git::https://github.com/manibhumaraju/terraform-aws-vpc.git?ref=main"
     #vpc_cidr = var.vpc_cidr
     project_name = var.project_name
     environment = var.environment_name
